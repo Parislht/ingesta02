bae7532a5882e6e93e127a35893a96ad59072561
Parislht <paris.herrera@utec.edu.pe>
2025-05-14 21:36:14 -0500

    Update ingesta.py

--- ingesta.py
+++ ingesta.py
@@ -1,9 +1,32 @@
+import mysql.connector
+import csv
 import boto3
 
-ficheroUpload = "data.csv"
-nombreBucket = "gcr-output-01"
+# Conexión a MySQL
+conexion = mysql.connector.connect(
+    host="mysql_c",  #nombre del contenedor de postgres
+    user="utec",
+    password="utec",
+    database="universidad"
+)
 
+cursor = conexion.cursor()
+cursor.execute("SELECT * FROM empleados")
+registros = cursor.fetchall()
+
+# Guardar en CSV
+nombre_csv = "empleados.csv"
+with open(nombre_csv, mode='w', newline='') as archivo:
+    writer = csv.writer(archivo)
+    writer.writerow(["id", "nombre", "puesto", "salario"])  
+    writer.writerows(registros)
+
+cursor.close()
+conexion.close()
+
+# Subir a S3
+nombre_bucket = "paris-output-01"  
 s3 = boto3.client('s3')
-response = s3.upload_file(ficheroUpload, nombreBucket, ficheroUpload)
+s3.upload_file(nombre_csv, nombre_bucket, nombre_csv)
 
-print("Ingesta completada")
+print("Ingesta desde MySQL completada.")
